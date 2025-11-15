6a9537cfffc8541e88326ae087a0c6e2902d3636
lucasew <lucas59356@gmail.com>
2025-11-15 13:19:48 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,7 @@
-import daisyui from "daisyui";
-import typography from "@tailwindcss/typography";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-  },
-  plugins: [typography, daisyui],
-  daisyui: {
-    themes: ["light", "dark"],
   },
 };
