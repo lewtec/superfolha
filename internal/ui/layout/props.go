824c81dae5dc16963eb7f0604ef42e15f2ed04af
lucasew <15693688+lucasew@users.noreply.github.com>
2026-08-26 17:27:43 +0000

    Updater script changes

--- internal/ui/layout/props.go
+++ internal/ui/layout/props.go
@@ -2,10 +2,10 @@ package layout
 
 // Chrome is shared by every HTML page.
 type Chrome struct {
-	Title    string
-	Lang     string
-	Flash    string
-	Error    string
+	Title        string
+	Lang         string
+	Flash        string
+	Error        string
 	LoggedIn     bool
 	Email        string
 	InviteAction string
